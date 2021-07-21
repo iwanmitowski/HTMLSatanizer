a8c817ddebb209de2b88071a7cd73afcd9082d49
iwanmitowski <iwan.mitowski@gmail.com>
2021-07-21 08:22:44 +0300

    Migrations with the updated model

---
+++ HTMLSatanizer/HTMLSatanizer/Migrations/20210721052157_updatingTheModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HTMLSatanizer.Migrations
+{
+    public partial class updatingTheModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Site",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Site",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HTMLSatanized",
+                table: "Site",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Site",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Site");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Site");
+
+            migrationBuilder.DropColumn(
+                name: "HTMLSatanized",
+                table: "Site");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Site");
+        }
+    }
+}
