42407340c5e736821eef667d238dece56d60605a
iwanmitowski <iwan.mitowski@gmail.com>
2021-07-28 16:37:50 +0300

    Animated hamburger icon

--- HTMLSatanizer/HTMLSatanizer/wwwroot/js/site.js
+++ HTMLSatanizer/HTMLSatanizer/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿$(function () {
+﻿//Converting datetime to users local time
+$(function () {
     $("time").each(function (i, e) {
         const dateTimeValue = $(e).attr("datetime");
         if (!dateTimeValue) {
@@ -8,5 +9,22 @@
         const time = moment.utc(dateTimeValue).local();
         $(e).html(time.format("llll"));
         $(e).attr("title", $(e).attr("datetime"));
+    });
+});
+
+//Navbar animation
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
     });
 });
